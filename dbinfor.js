ca2263ba772485628c143cda6ed65234f7962954
canon20paul <canon20paul@gmail.com>
2022-12-24 20:25:52 +0300

    initial

---
+++ dbinfor.js
@@ -0,0 +1,7 @@
+const db={
+    name:'localhost',
+    port:'5000',
+    username:'admin123',
+    password:'12345'
+}
+module.exports=db
---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+
+const dbinfor = require('./dbinfor')
+
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+mongoose.connect('mongodb://localhost:27017/sports',{useNewUrlParser:true, useUnifiedTopology:true}, function(){
+  try {
+    console.log('MongoDB Connected Successfully');
+  } catch (err) {
+    console.log(err);
+  }
+   })
+
+const GameModel = mongoose.model('games',{name:String, country:String, player:String})
+
+app.post('/addgame',(req,res)=>{
+     var name=req.body.name
+    var country = req.body.country
+    var player = req.body.player
+    var newgame=new GameModel({name:name, country:country, player:player})
+    newgame.save(function(error){
+       if(error){
+            res.send(error)
+        } else {
+                  res.send('Game Added Successfully to Database')
+        }
+    })
+})
+app.post('/deletegame', (req, res) => {
+    res.send('This is the Home Page')
+})
+app.post('/getgame', (req, res) => {
+    res.send('This is the Home Page')
+})
+app.post('/updategame', (req, res) => {
+    res.send('This is the Home Page')
+})
+
+app.listen(dbinfor.port, () => console.log(`Server running on Port: ${dbinfor.port}`))
+
